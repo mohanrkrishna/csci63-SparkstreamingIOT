a9ecbe4ef9b2b0e88a202310a110d920f0b6a6db
mohanrkrishna <emailmohan.it@gmail.com>
2017-05-05 16:45:51 -0400

    Add files via upload

--- spark_cassandra_flightdata.py
+++ spark_cassandra_flightdata.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext, Row
 from pyspark.streaming import StreamingContext
@@ -7,17 +6,29 @@ from pyspark.streaming.kafka import KafkaUtils
 import json
 import sys
 
-def savefunc(time, rdd):
+def loadcassandra(time, rdd,ctable):
    """
-    Invoked by foreach against dtream to loop through all rdds; Save to HDFS if not empty after bring data to single partition to avoid multiple files;
+    Invoked by foreach against dtream to loop through all rdds;
    """
    if (not rdd.isEmpty()):  #Check if rdd not empty to avoid empty files
        timesuffix = int(time.strftime('%s'))
-       filepath = "hdfs:///user/spark/projects/flightiot/output" + "-" + str(timesuffix)
-       df1=sql.read.json(rdd)
-       df2=df1.filter((df1.DepDelay !=0) | (df1.ArrDelay !=0))
-       df2.write.format('json').save(filepath)
-       print(str(time.strftime('%x %X')) + '...saving hdfs')
+       rdd.write.format("org.apache.spark.sql.cassandra"). \
+           options(table=ctable, keyspace="flightkeyspace"). \
+           save(mode="append")
+
+       print(str(time.strftime('%x %X')) + '...loading cassandra flightdata')
+
+
+def processdstream(rdd):
+    flightdelays_collist = [origin,flightnum,flightdate,email,origincityname,dest,destcityname,airtime,arrdelay,arrdelayminutes,arrivaldelaygroups,
+         arrtime,arrtimeblk,cancellationcode,cancelled,carrierdelay,departuredelaygroups,depdelay,depdelayminutes,deptime,deptimeblk,destairportid,deststate,
+         distance,distancegroup,diverted,first_name,flights,id,last_name,lateaircraftdelay,nasdelay,originairportid,originstate,phone,securitydelay,weatherdelay]
+
+    fstatusstream=sql.read.json(rdd)
+    fstatusstream_delay=fstatusstream.filter((fstatusstream.depdelay !=0) | (fstatusstream.arrdelay !=0))
+    customers_delay = fstatusstream_delay.join(customer,[flightnum,origin,dest],'inner'). \
+                      select(flightdelays_collist)
+    return customers_delay
 
 conf=SparkConf().setAppName("FlightIOTStreaming")
 sc = SparkContext(appName="FlightIOTStreaming")
@@ -29,14 +40,17 @@ brokers, topic = sys.argv[1:]
 flightstream = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
 #flightstream.pprint()
 flightdata = flightstream.map(lambda kv:kv[1])
-#flightdata = flightstream.map(lambda kv:json.loads(kv[1]))
-#                         .filter(lambda js: js['DepDelay']!=0 or js['ArrDelay']!=0 )
+
 
 flightwindow = flightdata.window(2,2)
 
-#tbl_flightiot = sql.read.format("org.apache.spark.sql.cassandra").\
-#               load(keyspace="flightiot", table="flightdata")
-flightwindow.foreachRDD(savefunc)
+customer = sql.read.format("org.apache.spark.sql.cassandra"). \
+           load(keyspace="flightkeyspace", table="customer").cache()
+
+customers_delay = flightwindow.transform(processdstream)
+
+flightwindow.foreachRDD(loadcassandra,"flightiot")
+customers_delay.foreachRDD(loadcassandra,"flighdelays")
 
 sc.setCheckpointDir("hdfs:///user/spark/projects/iotcheckpoint/")
 ssc.start()
